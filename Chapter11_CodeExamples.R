6fae46540aa4195206a5a1cc965a8e74d8297afe
Taweh Beysolow II <taweh.beysolowii@gmail.com>
2017-03-13 01:50:10 -0400

    Add files via upload

---
+++ Chapter11_CodeExamples.R
@@ -0,0 +1,184 @@
+#Introduction to Deep Learning 
+#Chapter 11 Examples 
+#Taweh Beysolow II 
+require(h2o)
+require(pROC)
+require(quantmod)
+require(MASS)
+require(LiblineaR)
+require(rpart)
+require(mlbench)
+require(caret)
+require(lmridge)
+require(e1071)
+require(Metrics)
+require(Amelia)
+require(randomForest)
+require(FSelector)
+require(RSNNS)
+require(ggplot2)
+require(lattice)
+require(nnet)
+require(pROC)
+require(ROCR)
+require(Amelia)
+require(varSelRF)
+require(caret)
+require(mclust)
+
+
+#Clear the workspace 
+rm(list = ls())
+
+
+#Function to be used later
+summaryStatistics <- function(array){
+  Mean <- mean(array)
+  Std <- sd(array)
+  Min <- min(array)
+  Max <- max(array)
+  Range <- Max - Min 
+  output <- data.frame("Mean" = Mean, "Std Dev" = Std, "Min" =  Min,"Max" = Max, "Range" = Range)
+  return(output)
+}
+
+#Upload the necessary data 
+data  <- read.csv("/Users/tawehbeysolow/Desktop/projectportfolio/SpeedDating.csv", header = TRUE, stringsAsFactors = TRUE)
+
+################################################################################################
+#Data Preprocessing
+#Creating response label 
+second_date  <- matrix(nrow = nrow(data), ncol = 1)
+
+for (i in 1:nrow(data)){
+  if (data[i,1] + data[i,2] == 2){
+    second_date[i]  <- 1
+  } else {
+    second_date[i]  <- 0
+  }
+}
+
+#Creating new data set 
+data  <- cbind(second_date, data)
+
+################################################################################################
+#Cleaning Data & Feature Transformation 
+
+#Finding NA Observations
+lappend <- function (lst, ...){
+  lst <- c(lst, list(...))
+  return(lst)
+}
+
+na_index <- list()
+for (i in 1:ncol(data)){
+  na_index <- lappend(na_index, which(is.na(data[,i])))
+}
+
+#Imputing NA Values where they are missing using EM Algorithm
+#Step 1: Label Encoding Factor Variables to prepare for input to EM Algorithm
+data$RaceM <- as.numeric(data$RaceM)
+data$RaceF <- as.numeric(data$RaceF)
+
+#Step 2: Inputting data to EM Algorithm 
+data <-  amelia(x = data, m = 1,  boot.type = "none")$imputations$imp1
+
+#Proof of EM Imputation 
+na_index <- list()
+for (i in 1:ncol(data)){
+  na_index <- lappend(na_index, which(is.na(data[,i])))
+}
+na_index <- matrix(na_index, ncol = length(na_index), nrow = 1)
+print(na_index)
+
+#Scaling Features
+data$AgeM <- scale(data$AgeM)
+data$AgeF <- scale(data$AgeF)
+
+################################################################################################
+#Feature Selection 
+corr <- cor(data)
+
+#Converting all Columns to Numeric prior to Input 
+for (i in 1:ncol(data)){
+  data[,i] <- as.integer(data[,i])
+}
+
+#Random Forest Feature Selection Based on Importance of Classification 
+data$second_date <- as.factor(data$second_date)
+featImport <- random.forest.importance(second_date ~., data = data, importance.type = 1)
+columns <- cutoff.k.percent(featImport, 0.4)
+print(columns)
+
+################################################################################################
+#Model Training 
+processedData <- data[, columns]
+
+#Bayes Classifier 
+AUC <- c()
+for (i in 1:100){
+  rows <- sample(1:nrow(processedData), 92)
+  bayesClass <- naiveBayes(y = as.factor(second_date[rows]), x = processedData[rows, ], data = processedData)
+  y_h <- predict(bayesClass, processedData[rows, ], type = c("class"))
+  AUC <- append(roc(y_h, as.numeric(second_date[rows]))$auc, AUC)
+}
+
+summaryStatistics(AUC)
+
+curve <- roc(y_h, as.numeric(second_date[rows]))
+roc(y_h, as.numeric(second_date[rows]))
+plot(curve, main = "Bayesian Classifier ROC")
+
+################################################################################################
+#Autoencoder
+h2o.init()
+
+training_data <- as.h2o(processedData, destination_frame = "train_data")
+
+autoencoder <- h2o.deeplearning(x = colnames(processedData), 
+                               training_frame = training_data, autoencoder = TRUE, activation = "Tanh",
+                               hidden = c(6,5,6), epochs = 100)
+
+
+#Reconstruct Original Data Set 
+syntheticData <- h2o.anomaly(autoencoder, training_data, per_feature = FALSE)
+errorRate <- as.data.frame(syntheticData)
+
+#Plotting Error Rate of Feature Reconstruction
+plot(sort(errorRate$Reconstruction.MSE), main = "Reconstruction Error Rate")
+
+
+################################################################################################
+#Removing Anomolies from Data 
+train_data <- processedData[errorRate$Reconstruction.MSE < 0.01, ]
+
+#Bayes Classifier 
+AUC <- c()
+for (i in 1:100){
+  rows <- sample(1:nrow(processedData), 92)
+  bayesClass <- naiveBayes(y = as.factor(second_date[rows]), x = processedData[rows, ], data = processedData)
+  y_h1 <- predict(bayesClass, processedData[rows, ], type = c("class"))
+  AUC <- append(roc(y_h1, as.numeric(second_date[rows]))$auc, AUC)
+}
+
+#Summary Statistics
+summaryStatistics(AUC)
+
+
+###############################################################################################
+#Using only Anomalies in Data Set
+train_data <- processedData[errorRate$Reconstruction.MSE >= 0.01, ]
+
+#Bayes Classifier 
+AUC <- c()
+for (i in 1:100){
+  rows <- sample(1:nrow(processedData), 92)
+  bayesClass <- naiveBayes(y = as.factor(second_date[rows]), x = processedData[rows, ], data = processedData)
+  y_h2 <- predict(bayesClass, processedData[rows, ], type = c("class"))
+  AUC <- append(roc(y_h2, as.numeric(second_date[rows]))$auc, AUC)
+}
+
+#Summary Statistics 
+summaryStatistics(AUC)
+
+
